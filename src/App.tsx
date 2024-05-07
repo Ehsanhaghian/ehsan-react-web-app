0734e27fa37e041cbae55b4d038b04feedd8e656
Ehsanhaghian <haghian.e@northeastern.edu>
2024-05-07 12:41:03 -0400

    Update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Labs from "./Labs";
+
+function App() {
+  return (
+    <div>
+      <h1>Jose Annunziato</h1>
+      <Labs />
+      {/* Kanbas */}
+      <a href="https://github.com/Ehsanhaghian/ehsan-react-web-app">
+        GitHub
+      </a>
+    </div>
+  );
+}
+
+export default App;
